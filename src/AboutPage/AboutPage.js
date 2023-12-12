ac199bb211c4e7d0f0dec28f591b134738671e32
Swapnasrivootla <svootla@uncc.edu>
2023-12-11 22:58:10 -0500

    update

---
+++ src/AboutPage/AboutPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function AboutPage() {
+  return (
+    <div>
+      <h1>About [PERSONAL-BUDGET-MANAGEMENT]</h1>
+      <p>
+        Welcome to [PERSONAL-BUDGET-MANAGEMENT]
+      </p>
+    </div>
+  );
+}
+
+export default AboutPage;
+
+
